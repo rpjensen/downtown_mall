5f49641324308f12af8351e320cc5ba752a210ed
Ryan Jensen <rjensen.professional@gmail.com>
2015-04-14 10:40:04 -0400

    Added building text highlighting

---
+++ DowntownMall/Assets/__Scripts/Building.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour {
+
+	public string buildingName;
+	public Text buildingLabel;
+
+
+	// Use this for initialization
+	void Start () {
+		buildingLabel = GameObject.Find ("BuildingLabel").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		print ("mouse in");
+		buildingLabel.text = buildingName;
+	}
+
+	void OnMouseExit() {
+		print ("mouse out");
+		if (buildingLabel.text == buildingName) {
+			buildingLabel.text = "";
+		}
+	}
+}
